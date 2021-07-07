7331c375e177593212f1c1cab58fc18012b90c7d
Administrator <Administrator@PC-WY012>
2021-07-07 09:55:44 +0800

    first

---
+++ src/main/java/cn/jbit/controller/UserController.java
@@ -0,0 +1,15 @@
+package cn.jbit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@RequestMapping("/sayHi")
+	public String sayHi() {
+		System.out.println("hello lxm!!");
+		return "index";
+	}
+}
